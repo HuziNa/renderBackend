4ad73f4e4b1cf80ac764ba2837ebc3113df6214d
ammarahmad01 <140069776+ammarahmad01@users.noreply.github.com>
2025-07-09 15:33:55 +0500

    Update Review.js
    Updated.

--- models/Review.js
+++ models/Review.js
@@ -1,19 +1,28 @@
 const mongoose = require('mongoose');
 
 const reviewSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  companyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true },
-  bookingId: { type: mongoose.Schema.Types.ObjectId, ref: 'Booking', required: true },
+  user: {
+    _id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    name: String
+  },
+  company: {
+    _id: { type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true },
+    name: String
+  },
+  booking: {
+    _id: { type: mongoose.Schema.Types.ObjectId, ref: 'Booking', required: true }
+  },
   rating: { type: Number, required: true, min: 1, max: 5 },
-  comment: { type: String, default: '' },
+  comment: String,
   isVisible: { type: Boolean, default: true },
   companyResponse: {
     message: String,
     respondedAt: Date
-  }
+  },
+  meta: mongoose.Schema.Types.Mixed
 }, { timestamps: true });
 
-reviewSchema.index({ companyId: 1 });
-reviewSchema.index({ userId: 1 });
+reviewSchema.index({ 'company._id': 1 });
+reviewSchema.index({ 'user._id': 1 });
 
 module.exports = mongoose.model('Review', reviewSchema);
